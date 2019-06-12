1a9f5115be90c51cb91ee8170748c55b9a8fbb31
jellevanderschaaf <50097870+jellevanderschaaf@users.noreply.github.com>
2019-06-12 18:47:10 +0200

    Made a connection to my diet_tracker database.

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+$con = mysqli_connect("localhost", "root", "", "diet_tracker");
+
+if(mysqli_connect_errno()) {
+    echo "Failed to connect: " . mysqli_connect_errno();
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>MySQL PHP</title>
+  </head>
+  <body>
+  
+
+
+  </body>
+</html>
