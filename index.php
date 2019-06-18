6ce31472a1da9c9303de5452da4dde18c998d27b
jellevanderschaaf <50097870+jellevanderschaaf@users.noreply.github.com>
2019-06-18 09:55:35 +0200

    Added radio buttons for selecting the correct measurement of the created food item (grams vs. piece).

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 session_start();
 $con = mysqli_connect("localhost", "root", "", "diet_tracker");
 
-if(mysqli_connect_errno()) {
+if (mysqli_connect_errno()) {
     echo "Failed to connect: " . mysqli_connect_errno();
 }
 
@@ -14,35 +14,34 @@ $kcals = "";
 $price = "";
 $error_array = "";
 
-if(isset($_POST['create_button'])){
+if (isset($_POST['create_button'])) {
 
-  $fname = strip_tags($_POST['reg_fname']);
-  $_SESSION['reg_fname'] = $fname;
-  $fat = strip_tags($_POST['reg_fat']);
-  $_SESSION['reg_fat'] = $fat;
-  $carbs = strip_tags($_POST['reg_carbs']);
-  $_SESSION['reg_carbs'] = $carbs;
-  $protein = strip_tags($_POST['reg_protein']);
-  $_SESSION['reg_protein'] = $protein;
-  $kcals = strip_tags($_POST['reg_kcals']);
-  $_SESSION['reg_kcals'] = $kcals;
-  $price = strip_tags($_POST['reg_price']);
-  $_SESSION['reg_price'] = $price;
+    $fname = strip_tags($_POST['reg_fname']);
+    $_SESSION['reg_fname'] = $fname;
+    $fat = strip_tags($_POST['reg_fat']);
+    $_SESSION['reg_fat'] = $fat;
+    $carbs = strip_tags($_POST['reg_carbs']);
+    $_SESSION['reg_carbs'] = $carbs;
+    $protein = strip_tags($_POST['reg_protein']);
+    $_SESSION['reg_protein'] = $protein;
+    $kcals = strip_tags($_POST['reg_kcals']);
+    $_SESSION['reg_kcals'] = $kcals;
+    $price = strip_tags($_POST['reg_price']);
+    $_SESSION['reg_price'] = $price;
 
-  // Check if fname already exists
+    // Check if fname already exists
 
-$query = "SELECT count(*) as allcount FROM food_items WHERE fname='".$fname."'";
-$result = mysqli_query($con,$query);
-$row = mysqli_fetch_array($result);
-$allcount = $row['allcount'];
+    $query = "SELECT count(*) as allcount FROM food_items WHERE fname='" . $fname . "'";
+    $result = mysqli_query($con, $query);
+    $row = mysqli_fetch_array($result);
+    $allcount = $row['allcount'];
 
-  // insert
+    // insert
 
-  if(empty($error_array) && $allcount == 0) {
-  
-  $query = mysqli_query($con, "INSERT INTO food_items VALUES ('', '$fname', '$fat', '$carbs', '$protein', '$kcals', '$price')");
-  
-}
+    if (empty($error_array) && $allcount == 0) {
+
+        $query = mysqli_query($con, "INSERT INTO food_items VALUES ('', '$fname', '$fat', '$carbs', '$protein', '$kcals', '$price')");
+    }
 }
 
 
@@ -78,39 +77,43 @@ $allcount = $row['allcount'];
                 <form id="foodForm" action="index.php" method="POST">
                     <div class="form-group">
 
-                    <table style="width:100%">
-                    <tr>
-    <td>name</td>
-    <td><input id="fname" name="reg_fname" type="text" class="form-control form-control-sm formFoodItem" required></td> 
-    <td></td>
-  </tr>
-  <tr>
-    <td>fat</td>
-    <td><input id="fat" name="reg_fat" type="text" class="form-control form-control-sm formFoodItem" required></td> 
-    <td>grams</td>
-  </tr>
-  <tr>
-    <td>carbs</td>
-    <td><input id="carbs" name="reg_carbs" type="text" class="form-control form-control-sm formFoodItem" required></td> 
-    <td>grams</td>
-  </tr>
-  <tr>
-    <td>protein</td>
-    <td><input id="protein" name="reg_protein" type="text" class="form-control form-control-sm formFoodItem" required></td> 
-    <td>grams</td>
-  </tr>
-  <tr>
-    <td>kcals</td>
-    <td><input id="kcals" name="reg_kcals" type="text" class="form-control form-control-sm formFoodItem" required></td> 
-    <td></td>
-  </tr>
-  <tr>
-    <td>price: €</td>
-    <td><input id="price" name="reg_price" type="text" class="form-control form-control-sm formFoodItem" required></td> 
-    <td></td>
-  </tr>
-  
-</table>     
+                        <table style="width:100%">
+                            <tr>
+                                <td>name</td>
+                                <td><input id="fname" name="reg_fname" type="text" class="form-control form-control-sm formFoodItem" required></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>fat</td>
+                                <td><input id="fat" name="reg_fat" type="text" class="form-control form-control-sm formFoodItem" required></td>
+                                <td>grams</td>
+                            </tr>
+                            <tr>
+                                <td>carbs</td>
+                                <td><input id="carbs" name="reg_carbs" type="text" class="form-control form-control-sm formFoodItem" required></td>
+                                <td>grams</td>
+                            </tr>
+                            <tr>
+                                <td>protein</td>
+                                <td><input id="protein" name="reg_protein" type="text" class="form-control form-control-sm formFoodItem" required></td>
+                                <td>grams</td>
+                            </tr>
+                            <tr>
+                                <td>kcals</td>
+                                <td><input id="kcals" name="reg_kcals" type="text" class="form-control form-control-sm formFoodItem" required></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>price: €</td>
+                                <td><input id="price" name="reg_price" type="text" class="form-control form-control-sm formFoodItem" required></td>
+                                <td></td>
+                            </tr>
+
+                        </table>
+
+                        list per:<br>
+                        100 grams <input type="radio" name="grams" value="grams"><br>
+                        piece <input type="radio" name="piece" value="piece">
                     </div>
                     <button class="btn btn-secondary" onClick="cancel()">Cancel</button>
                     <button type="submit" name="create_button" value="Create" class="btn btn-secondary" onClick="createItem()">Create</button>
@@ -121,27 +124,27 @@ $allcount = $row['allcount'];
 
             <div id="test" class="foodList">
 
-        
-            <table>
-<tr>
-</tr>
-<?php
 
-$sql = "SELECT id, fname from food_items";
-$result = $con-> query($sql);
+                <table>
+                    <tr>
+                    </tr>
+                    <?php
 
-if ($result-> num_rows > 0) {
-    while ($row = $result-> fetch_assoc()) {
-        echo "<tr><td>" . $row["fname"] ."</td></tr>";
-    } 
-    }
-    $con-> close();
+                    $sql = "SELECT id, fname from food_items";
+                    $result = $con->query($sql);
 
-?>
+                    if ($result->num_rows > 0) {
+                        while ($row = $result->fetch_assoc()) {
+                            echo "<tr><td>" . $row["fname"] . "</td></tr>";
+                        }
+                    }
+                    $con->close();
+
+                    ?>
+
+            </div>
 
         </div>
-        
-    </div>
 
 
 
@@ -149,10 +152,10 @@ if ($result-> num_rows > 0) {
 
 
 
-    <script type="text/javascript" src="javascript.js"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="javascript.js"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 </body>
 
