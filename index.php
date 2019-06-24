92ef748c773f9f4c65a3458a1977f40d9d8dce5f
jellevanderschaaf <50097870+jellevanderschaaf@users.noreply.github.com>
2019-06-24 14:28:52 +0200

    Added a font awesome delete icon.

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ if (isset($_POST['create_button'])) {
         <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
@@ -137,7 +137,7 @@ if (isset($_POST['create_button'])) {
                             echo "<tr>";
 
                             echo "<td>{$row['fname']}</td>";
-                            echo "<td><a rel='".$row['id']."' class='delete-this' href='javascript:void(0)'>d</a></td>";
+                            echo "<td><a rel='".$row['id']."' class='delete-this' href='javascript:void(0)'><i style='font-size:24px' class='fa'>&#xf014;</i></a></td>";
                             echo "<td><input type='button' class='btn btn-danger add' value='add'></td>";
                 
                             echo "</tr>";
